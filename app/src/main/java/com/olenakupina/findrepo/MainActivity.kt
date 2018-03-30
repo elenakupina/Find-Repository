75061df02f38a117c6297c08aaab04d48aa56029
Elena <kupina09011982@gmail.com>
2018-03-29 17:35:24 -0700

    created a project

---
+++ app/src/main/java/com/olenakupina/findrepo/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.olenakupina.findrepo
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        val userReoEditText = findViewById<EditText>(R.id.userRepoEditText)
+
+        val button = findViewById<Button>(R.id.searchButton)
+        button.setOnClickListener {
+            val intent = Intent(this, SearchResultActivity::class.java)
+            intent.putExtra("searchTerm", searchEditText.text.toString())
+            startActivity(intent)
+        }
+        val viewRepoButton = findViewById<Button>(R.id.userRepoButton)
+        viewRepoButton.setOnClickListener{
+            val intent = Intent(this, SearchResultActivity::class.java)
+            intent.putExtra("username", userReoEditText.text.toString())
+            startActivity(intent)
+        }
+    }
+}
