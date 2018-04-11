36202b9596e4d7a1855b12e01755298e55faceba
Elena <kupina09011982@gmail.com>
2018-04-10 23:37:48 -0700

    Added fabric dependencies and properties

--- app/src/main/java/com/olenakupina/findrepo/MainActivity.kt
+++ app/src/main/java/com/olenakupina/findrepo/MainActivity.kt
@@ -5,11 +5,16 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
 
         val searchEditText = findViewById<EditText>(R.id.searchEditText)
